718b2e60226a386b9b90c3569c2edb9344b4cb5e
roseak <rose.kohn@gmail.com>
2015-10-27 07:10:11 -0600

    Finally got oauth working! and added style to the welcome.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ActiveRecord::Base
   def self.from_oauth(auth_info)
-    where(uid: auth_info[:uid]).first_or_create do |new_user|
-      new_user.name = auth_info.info.name
-      new_user.nickname = auth_info.info.nickname
-      new_user.image = auth_info.info.image
-      new_user.bio = auth_info.info.bio
-      new_user.website = auth_info.info.website
-      new_user.token = auth_info.credentials.token
-      new_user.uid = auth_info.uid
+    where(uid: auth_info[:id]).first_or_create do |new_user|
+      new_user.uid = auth_info["user"]["id"]
+      new_user.name = auth_info["user"]["full_name"]
+      new_user.image = auth_info["user"]["profile_picture"]
+      new_user.bio = auth_info["user"]["bio"]
+      new_user.website = auth_info["user"]["website"]
+      new_user.token = auth_info["access_token"]
     end
   end
 end
